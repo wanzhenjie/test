c2a435da4873e319879d62b7b151a0640c246ced
wanzhenjie <410597886@qq.com>
2019-02-17 11:05:14 +0800

    第三次提交

--- src/test.java
+++ src/test.java
@@ -3,4 +3,9 @@ public class test {
     public String toString() {
         return super.toString();
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
 }
